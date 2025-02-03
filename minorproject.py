e02ce4948ddd38c0f533ccbcc156c93884f9e3d3
Bharathreddy2004 <151860019+Bharathreddy2004@users.noreply.github.com>
2025-02-03 21:43:32 +0530

    Update minorproject.py

--- minorproject.py
+++ minorproject.py
@@ -1,18 +1,7 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 import numpy as np
-st.title("hello")
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            iframe {
-                    border: none;
-            }
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+
 #st.markdown(hide_st_style, unsafe_allow_html=True)
 # power consumption per hour of appliances (in watts)
 COMPUTER_POWER_PER_HOUR = 250
